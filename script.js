34855155e266570ebff57a6a13ee3789d262d9f5
Norival <norivaljunior.jf@gmail.com>
2022-06-24 00:33:52 -0300

    recarregando msg

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let user;
 let nome;
 let mensagemTemplate;
 entradaNaSala();
-/* setInterval(atualizarStatus, 5000); */
+
 
 function entradaNaSala() {
     nome = prompt("Qual a sua graça ?");
@@ -12,14 +12,24 @@ function entradaNaSala() {
     }
     const envio = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', user);
     envio.then(carregarMensagens);
+    envio.catch(erroNome)
 }
 
-function atualizarStatus (){
+function erroNome() {
+    alert("Esse nome está inválido, por favor digie um nome válido");
+    window.location.reload()
+}
+
+function atualizarStatus() {
     let online = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', user);
-  
+
 }
 
 function carregarMensagens() {
+    resposta = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    resposta.then(exibirMensagens)
+}
+function recarregarMensagens (){
     resposta = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     resposta.then(exibirMensagens)
 }
@@ -29,20 +39,32 @@ function exibirMensagens(resposta) {
     mensagens = resposta.data;
 
     for (let i = 0; i < mensagens.length; i++) {
-       
+
 
         if (mensagens[i].type === 'status') {
-        mensagemTemplate = `<li class="${mensagens[i].type}"><span class = "time">(${mensagens[i].time}) </span>
-        <span class = "from negrito">${mensagens[i].from}</span> 
-        <span class = "mensagem">${mensagens[i].text}</span></li>`
-        
+            mensagemTemplate = `<li class="${mensagens[i].type}">
+            <span class = "time">(${mensagens[i].time}) </span>
+            <span class = "from negrito">${mensagens[i].from}</span> 
+            <span class = "mensagem">${mensagens[i].text}</span></li>`
+
         } else if (mensagens[i].type === 'message') {
-            mensagemTemplate = `<li class="${mensagens[i].type}"><span class = "time">(${mensagens[i].time}) </span>
-        <span class = "from negrito">${mensagens[i].from}</span> 
-        <span class = "mensagem">${mensagens[i].text}</span></li>`
+            mensagemTemplate = `<li class="${mensagens[i].type}">
+            <span class = "time">(${mensagens[i].time}) </span>
+            <span class = "from negrito">${mensagens[i].from}</span> 
+            <span class = "mensagem">${mensagens[i].text}</span></li>`
+        } else if (user.name === mensagens[i].from || user.name === mensagens[i].to) {
+            mensagemTemplate = `<li class="${mensagens[i].type}">
+            <span class = "time">(${mensagens[i].time}) </span>
+            <span class = "from negrito">${mensagens[i].from}
+            </span> <span class= "to"> Reservardamente para ${mensagem[i].to}</span>
+            <span class = "mensagem">${mensagens[i].text}</span></li>`
+
         }
-        document.querySelector(".bate-papo").innerHTML +=`${mensagemTemplate}`
+        document.querySelector(".bate-papo").innerHTML += `${mensagemTemplate}`
 
     }
 
-}
+} 
+/* setInterval(atualizarStatus, 5000); */
+setInterval (recarregarMensagens,3000);
+
